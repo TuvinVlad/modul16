f7cd825ff7624b413c7b0e20c55021c89b066f23
TuvinVlad <101350492+TuvinVlad@users.noreply.github.com>
2022-05-11 20:15:58 +0300

    Task16.8.2_1

---
+++ rectangle.py
@@ -0,0 +1,25 @@
+class Rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_area(self):
+        return self.a * self.b
+
+
+class Square:
+    def __init__(self, a):
+        self.a = a
+
+    def get_area_square(self):
+        return self.a ** 2
+
+
+class Circle:
+    def __init__(self, r, pi=3.14):
+        self.pi = pi
+        self.r = r
+
+    def get_area_circle(self):
+        return self.pi * self.r ** 2
+    
