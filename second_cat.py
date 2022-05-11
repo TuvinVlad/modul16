19b5106bd3eaf06161dd68956a39c076e74e1596
TuvinVlad <101350492+TuvinVlad@users.noreply.github.com>
2022-05-11 20:13:11 +0300

    Task16.7.1_2

---
+++ second_cat.py
@@ -0,0 +1,8 @@
+from maincats import Cat
+
+cat1 = Cat("Барон", "мальчик", 2)
+cat2 = Cat("Сэм", "мальчик", 2)
+
+print(f'Имя питомца - {cat1.name}, пол - {cat1.gender}, возраст - {cat1.age}')
+
+print(f'Имя питомца - {cat2.name}, пол - {cat2.gender}, возраст  - {cat2.age}')
